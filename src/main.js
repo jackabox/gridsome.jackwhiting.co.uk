773680e19db259578f7adefa0e9e6d197935a09c
Jack Whiting <jack.w@weareframework.co.uk>
2019-02-22 17:26:17 +0000

    Init blog

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from "~/layouts/Default.vue";
+
+export default function(Vue, { head }) {
+  // Set default layout as a global component
+  Vue.component("Layout", DefaultLayout);
+
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css?family=Mali:700|Nunito"
+  });
+}

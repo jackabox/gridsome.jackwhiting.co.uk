7b8846d7e6c5fbc46b1c56a51e573829ecb3f408
Jack Whiting <hi@jackwhiting.co.uk>
2019-02-27 13:54:16 +0000

    minor tweaks, links etc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,26 +11,36 @@
           </p>
 
           <p>
-            Currently, I work for
+            I work for
             <a
               href="https://weareframework.co.uk"
               target="_blank"
               rel="noreferer noopener"
-              class="text-black border-b border-pink"
+              class="text-black border-b border-pink hover:bg-pink-lighter hover:border-pink-lighter"
               title="Link to Framework Design's website"
-            >Framework Design</a> - a small digital agency in the heart of Nottingham - where I specialise in crafting larger applications written with Laravel (PHP) and VueJs (JavaScript). My other tasks often involve scoping projects, writing APIs or drinking gallons of Tea.
+            >Framework Design</a> - a small digital agency in the heart of Nottingham - where I specialise in crafting larger applications written with Laravel (PHP) and Vue (JavaScript). My other tasks often involve scoping projects, writing APIs or drinking gallons of Tea.
           </p>
 
           <p>
             I dabble in the odd freelance project, as long as it's awesome, to get in touch
             <a
               href="mailto:hi@jackwhiting.co.uk"
-              class="text-black border-b border-pink"
+              class="text-black border-b border-pink hover:bg-pink-lighter hover:border-pink-lighter"
               title="Email Jack about Freelance"
             >email me</a>.
           </p>
 
-          <p>Otherwise, you can find me with my family, at gigs or cooking up a storm. I also have a personal battle with Crohn's Disease after being diagnosed at the age of 18.</p>
+          <p>
+            Otherwise, you can find me with my family, at gigs or
+            <s class="text-grey">burning</s> cooking food. I also have a daily battle with
+            <a
+              href="https://www.crohnsandcolitis.org.uk/about-inflammatory-bowel-disease/crohns-disease"
+              target="_blank"
+              rel="noreferer noopener"
+              class="text-black border-b border-pink hover:bg-pink-lighter hover:border-pink-lighter"
+              title="Link to find out more about Crohn's"
+            >Crohn's Disease</a> after being diagnosed at the age of 18.
+          </p>
         </slide-in>
 
         <div class="w-full md:w-6/12 mt-5 column">

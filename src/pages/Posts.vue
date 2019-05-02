6c244b503dea85465399270db96f557574410040
Jack Whiting <hi@jackwhiting.co.uk>
2019-05-02 20:31:56 +0100

    update to site with works

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -2,25 +2,27 @@
   <Layout>
     <page-header title="Posts"/>
 
-    <ul class="list-reset">
-      <li v-for="({node}, index) in $page.allPost.edges" :key="node._id" class="py-10">
-        <slide-in :reverse="true" :delay="200 * parseInt(index)">
-          <router-link :to="node.path" :title="'Link to post: ' + node.title">
-            <h2 v-html="node.title"/>
-          </router-link>
+    <div class="container">
+      <ul class="list-reset">
+        <li v-for="({node}, index) in $page.allPost.edges" :key="node._id" class="py-10">
+          <slide-in :reverse="true" :delay="200 * parseInt(index)">
+            <router-link :to="node.path" :title="'Link to post: ' + node.title">
+              <h2 v-html="node.title"/>
+            </router-link>
 
-          <div class="mt-3 mb-2 text-sm tracking-wide uppercase text-grey-darker">{{ node.date }}</div>
+            <div class="mt-3 mb-2 text-sm tracking-wide uppercase text-grey-darker">{{ node.date }}</div>
 
-          <div v-html="node.description"/>
+            <div v-html="node.description"/>
 
-          <router-link :to="node.path" class="mt-5 block" :title="'Link to post: ' + node.title">
-            <span
-              class="border border-2 border-pink inline-flex pb-1 uppercase text-sm text-grey-darker hover:text-black hover:bg-pink px-2 py-1 tracking-wide"
-            >Read More</span>
-          </router-link>
-        </slide-in>
-      </li>
-    </ul>
+            <router-link :to="node.path" class="mt-5 block" :title="'Link to post: ' + node.title">
+              <span
+                class="border border-2 border-pink inline-flex pb-1 uppercase text-sm text-grey-darker hover:text-black hover:bg-pink px-2 py-1 tracking-wide"
+              >Read More</span>
+            </router-link>
+          </slide-in>
+        </li>
+      </ul>
+    </div>
   </Layout>
 </template>
 
@@ -48,10 +50,6 @@ export default {
   components: {
     PageHeader,
     SlideIn
-  },
-
-  mounted() {
-    console.table(this.$page.allPost.edges)
   },
 
   metaInfo() {

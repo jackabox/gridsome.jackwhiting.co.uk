d38199ad45dc27a65fd430a54addf3c8cf312cbd
Jack Whiting <hi@jackwhiting.co.uk>
2019-09-17 12:22:24 +0100

    Adding 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <slide-in :reverse="true" class="container pt-20 mt-20">
+      <h1>Error 404</h1>
+      <p>Sorry, that page cannot be found.</p>
+      <p>
+        <g-link to="/" class="btn mr-5">Home</g-link>
+        <g-link to="/services" class="btn">See What I Do</g-link>
+      </p>
+    </slide-in>
+  </Layout>
+</template>
+
+<script>
+import SlideIn from '~/components/Animation/SlideIn'
+
+export default {
+  components: {
+    SlideIn
+  }
+}
+</script>
